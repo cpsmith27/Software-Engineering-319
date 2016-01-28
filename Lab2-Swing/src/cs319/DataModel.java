eed11fc621b840809a60f2622bfe7762f9f884da
borisn2 <borisn@iastate.edu>
2016-01-27 22:11:14 -0600

    Update with Boris's code.

---
+++ Lab2-Swing/src/cs319/DataModel.java
@@ -0,0 +1,66 @@
+package cs319;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class DataModel extends javax.swing.AbstractListModel {
+	
+
+ private static ArrayList<String> list_companies=  new ArrayList<String>();
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		
+		String s="/Users/BORISNDOUTOUME/Documents/workspace/SE319/Software-Engineering-319/Lab2/companies.txt";
+		ArrayList<String> arr= new ArrayList<String>();
+		arr= readfiles(s);
+		
+		for (int i=0; i<arr.size();i++){
+			System.out.println(arr.get(i));
+		}
+		
+		
+		
+	}
+	
+	
+	public static ArrayList<String> readfiles (String filename) throws FileNotFoundException{ 
+	
+	File file = new File(filename);
+	
+	Scanner sc = new Scanner(file);
+	
+	
+	while(sc.hasNext()){
+		String s = sc.next();
+		list_companies.add(s);
+		
+	}
+	
+	
+	
+	return list_companies;
+	
+
+}
+	
+	
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return list_companies.size();
+	}
+
+	@Override
+	public Object getElementAt(int index) {
+		// TODO Auto-generated method stub
+		return list_companies.get(index);
+	}
+	
+	
+	
+
+}
