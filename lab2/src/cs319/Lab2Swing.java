4631818728a48ed18019851447d54ecda0e7d3cd
cpsmith27 <cpsmith@iastate.edu>
2016-01-29 03:09:50 -0600

    Working part 2.

--- lab2/src/cs319/Lab2Swing.java
+++ lab2/src/cs319/Lab2Swing.java
@@ -8,6 +8,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeModel;
 import javax.swing.JTabbedPane;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
@@ -17,6 +20,7 @@ import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
+import javax.swing.JTree;
 
 public class Lab2Swing extends JFrame {
 
@@ -60,7 +64,7 @@ public class Lab2Swing extends JFrame {
 		panel_1.setLayout(null);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(6, 6, 406, 173);
+		scrollPane.setBounds(6, 6, 405, 175);
 		panel_1.add(scrollPane);
 		
 		JList<String> list = new JList<String>();
@@ -100,10 +104,52 @@ public class Lab2Swing extends JFrame {
 		panel_1.add(btnNewButton_1);
 		
 		JPanel panel_2 = new JPanel();
-		tabbedPane.addTab("Table", null, panel_2, null);
+		tabbedPane.addTab("Tree", null, panel_2, null);
+		panel_2.setLayout(null);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(6, 6, 405, 175);
+		panel_2.add(scrollPane_1);
+		
+		JTree tree = new JTree();
+		DefaultTreeModel treeModel = new DefaultTreeModel(new AnimalsTree());
+		tree.setModel(treeModel);
+		scrollPane_1.setViewportView(tree);
+		
+		JButton btnNewButton_2 = new JButton("Add");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+				if (node != null) {
+					NewNodeDialog dialog = new NewNodeDialog(node, treeModel);
+					dialog.setVisible(true);
+				} else {
+					NewNodeDialog dialog = new NewNodeDialog((DefaultMutableTreeNode) treeModel.getRoot(), treeModel);
+					dialog.setVisible(true);
+				}
+			}
+		});
+		btnNewButton_2.setBounds(71, 185, 117, 29);
+		panel_2.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Remove");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+				if (node != null) {
+					if (node == treeModel.getRoot()) {
+						treeModel.setRoot(null);
+					} else {
+						treeModel.removeNodeFromParent(node);
+					}
+				}
+			}
+		});
+		btnNewButton_3.setBounds(227, 185, 117, 29);
+		panel_2.add(btnNewButton_3);
 		
 		JPanel panel = new JPanel();
-		tabbedPane.addTab("Tree", null, panel, null);
+		tabbedPane.addTab("Table", null, panel, null);
 	}
 	
 	public JScrollPane getScrollPane() {
